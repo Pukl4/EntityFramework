3e35ae74ca4d1d770ab806a9dd9250082436ec3e
Puki4 <nikitka2601@mail.ru>
2024-10-05 22:41:49 +0300

    Fix pull request comment

--- EFWorkshop/EFWorkshopCodeFirst/DataAccess/Models/Student.cs
+++ EFWorkshop/EFWorkshopCodeFirst/DataAccess/Models/Student.cs
@@ -3,7 +3,6 @@
     internal class Student
     {
         public int StudentId { get; set; }
-
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
